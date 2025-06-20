7481aeaad6b9e654d7f825abbb46cd72ca4d8df4
Madhu Vivek Borra <madhuvivekborra4@gmail.com>
2025-06-20 17:02:18 -0400

    Fix RDS engine and instance type for compatibility

--- src/CaseGuardInfra/DatabaseStack.cs
+++ src/CaseGuardInfra/DatabaseStack.cs
@@ -9,25 +9,41 @@ namespace CaseGuardInfra
     {
         public DatabaseStack(Construct scope, string id, StackProps props = null) : base(scope, id, props)
         {
-            var vpc = Vpc.FromLookup(this, "Vpc", new VpcLookupOptions { IsDefault = true });
+            var vpc = Vpc.FromLookup(this, "Vpc", new VpcLookupOptions
+            {
+                IsDefault = true
+            });
+
+            var subnetGroup = new SubnetGroup(this, "RdsSubnetGroup", new SubnetGroupProps
+            {
+                Description = "Subnet group for RDS instance",
+                Vpc = vpc,
+                VpcSubnets = new SubnetSelection { SubnetType = SubnetType.PUBLIC },
+                RemovalPolicy = RemovalPolicy.DESTROY
+            });
 
             new DatabaseInstance(this, "RdsInstance", new DatabaseInstanceProps
             {
                 Engine = DatabaseInstanceEngine.Postgres(new PostgresInstanceEngineProps
                 {
-                    Version = PostgresEngineVersion.VER_15
+                    // Downgraded to version 14, which is widely supported
+                    Version = PostgresEngineVersion.VER_14
                 }),
+                InstanceType = InstanceType.Of(InstanceClass.BURSTABLE3, InstanceSize.MICRO), // t3.micro is free tier eligible and supported
                 Vpc = vpc,
-                InstanceType = Amazon.CDK.AWS.EC2.InstanceType.Of(InstanceClass.BURSTABLE2, InstanceSize.SMALL),
-                Credentials = Credentials.FromGeneratedSecret("admin"),
-                MultiAz = false,
-                AllocatedStorage = 20,
                 VpcSubnets = new SubnetSelection
                 {
                     SubnetType = SubnetType.PUBLIC
                 },
+                SubnetGroup = subnetGroup,
+                Credentials = Credentials.FromGeneratedSecret("admin"),
+                MultiAz = false,
+                AllocatedStorage = 20,
+                BackupRetention = Duration.Days(0),
+                DeletionProtection = false,
                 RemovalPolicy = RemovalPolicy.DESTROY
             });
         }
     }
 }
+
